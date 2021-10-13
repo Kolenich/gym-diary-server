17a3435f9618266345ea5262441e842fd6e58713
Kolenich <nick.zhigalin@gmail.com>
2021-10-13 23:48:05 +0300

    Added Workout and Exercise models.

--- workout/models.py
+++ workout/models.py
@@ -1,1 +1,18 @@
 """Модели приложения workout."""
+from django.db import models
+
+
+class Workout(models.Model):
+    """Модель тренировки."""
+    date = models.DateField()
+    start = models.TimeField(null=True, blank=True)
+    end = models.TimeField(null=True, blank=True)
+
+
+class Exercise(models.Model):
+    """Модель упражнения."""
+
+    name = models.CharField(max_length=128)
+    weight = models.PositiveSmallIntegerField()
+    repeats = models.PositiveSmallIntegerField()
+    workout = models.ForeignKey('Workout', on_delete=models.CASCADE)
