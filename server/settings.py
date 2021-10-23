e72c7bfbf3787cc99db9ccfdbf07a0500d0a4ef3
Kolenich <nick.zhigalin@gmail.com>
2021-10-23 15:04:24 +0300

    Adjusted some prod settings.

--- server/settings.py
+++ server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%336)$s02pvrh+d@pnzjp(7z)v3**5c-gcj56o1jdg7447pw8*'
+SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'true').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = list(filter(bool, os.getenv('ALLOWED_HOSTS', '*').split(',')))
 
 # Application definition
 
