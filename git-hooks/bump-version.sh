89ff13ec52f719f9a687c7bd4357a1986c7d803f
Kolenich <zhigalin.nick@yandex.ru>
2025-09-17 00:17:06 +0300

    Added VCS.

---
+++ git-hooks/bump-version.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python git-hooks/bump_version.py patch
+#git add settings.ini
---
+++ git-hooks/bump_version.py
@@ -0,0 +1,30 @@
+import sys
+from configparser import ConfigParser
+
+
+def bump_version():
+    config = ConfigParser()
+    config.read('settings.ini')
+
+    version = config['SETTINGS']['version']
+
+    major, minor, patch = map(int, version.split('.'))
+
+    action = sys.argv[1]
+    if action == 'major':
+        major, minor, patch = major + 1, 0, 0
+    elif action == 'minor':
+        minor, patch = minor + 1, 0
+    elif action == 'patch':
+        patch = patch + 1
+
+    version = f'{major}.{minor}.{patch}'
+
+    config['SETTINGS']['version'] = version
+
+    with open('../settings.ini', 'w') as configfile:
+        config.write(configfile)
+
+
+if __name__ == '__main__':
+    bump_version()
