5f571d9604223a813b1834dab8a4ba53162a0911
Kolenich <zhigalin.nick@yandex.ru>
2025-09-17 03:35:37 +0300

    Updated static URL.

--- server/settings.py
+++ server/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/django-static/'
-STATIC_ROOT = BASE_DIR / 'django-static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
