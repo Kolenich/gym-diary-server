034feec02c914b9b978f89f720784b40f8a18b7b
Kolenich <zhigalin.nick@yandex.ru>
2025-10-04 03:43:17 +0300

    Updated static url.

--- server/settings.py
+++ server/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'ru'
-TIME_ZONE = tzlocal.get_localzone_name()
+TIME_ZONE = os.getenv('TZ', tzlocal.get_localzone_name())
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
